8029afc8419a7d5f61512e89dc50cc3d5c79982f
Ruby Yeh <hyoka4654@gmail.com>
2022-01-27 11:20:07 +0800

    [test] 測試azure連線Mysql資料庫

--- db/database.py
+++ db/database.py
@@ -1,8 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./nstar.db"
+DB_USERNAME = os.environ.get("nsatr")
+DB_PASSWORD = os.environ.get("Password1")
+DB_NAME = os.environ.get("testserver-mysql.mysql.database.azure.com")
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_NAME}"
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./nstar.db"
 
 newengine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
